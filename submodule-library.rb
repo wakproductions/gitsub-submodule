60c13b2fcfa7be3d8e426c9e57894dad056bec4f
Winston Kotzan <wak@wakproductions.com>
2016-09-26 17:45:08 -0400

    New method and modifications from parent repo

--- submodule-library.rb
+++ submodule-library.rb
@@ -5,7 +5,11 @@ class MySubmoduleLibrary
   end
 
   def code_change_in_shared_repo
-    puts "This code was added from the shared repo"
+    puts "This code was modified from the parent repo"
+  end
+
+  def code_change_from_parent_repo
+    puts "This code was added from the parent repo"
   end
 
 end
